97037ba03c661e7176f924b7085406e6394e1e99
Hassan Juma <okothhassanjuma@gmail.com>
2020-11-29 22:47:34 +0300

    add urls.py

---
+++ awwards/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home_page,name='home_page'),
+    path('project/new',views.submit_project,name='submit_project'),
+    path('search/projects',views.search_projects,name='search_projects'),
+    path('view/project/<prj_id>',views.view_project,name='view_project'),
+    path('view/my-profile',views.my_profile,name='my_profile'),
+    path('api/profiles', views.ProfileList.as_view()),
+    path('api/projects', views.ProjectsList.as_view()),
+]	
