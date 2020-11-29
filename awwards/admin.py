1052f1a5ebbafd1415eebb083d80e146add615e6
Hassan Juma <okothhassanjuma@gmail.com>
2020-11-29 22:46:02 +0300

    add admin

---
+++ awwards/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Profile,Project,Rating
+
+# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Rating)
